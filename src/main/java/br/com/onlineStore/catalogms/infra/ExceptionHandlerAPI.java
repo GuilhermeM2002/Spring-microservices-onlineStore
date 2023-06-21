376b52ad460b2e27c36ccec7ebe22a7ab090981f
Guilherme Martins Ribeiro <guilhermemartinsribeiro2002@gmail.com>
2023-06-20 22:25:41 -0300

    Added exception handler

---
+++ src/main/java/br/com/onlineStore/catalogms/infra/ExceptionHandlerAPI.java
@@ -0,0 +1,26 @@
+package br.com.onlineStore.catalogms.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerAPI {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404(){
+        return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity error404(MethodArgumentNotValidException ex){
+        var error = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(error.stream().map(DataError::new).toList());
+    }
+    public record DataError(String field, String message){
+        public DataError(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
