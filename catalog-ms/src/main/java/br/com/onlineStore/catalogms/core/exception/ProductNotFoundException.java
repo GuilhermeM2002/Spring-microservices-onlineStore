e1b5eb95b7c7ecc9527568d25c231b1d133905dd
Guilherme Martins <guilhermemartinsribeiro2002@gmail.com>
2023-09-12 18:07:49 -0300

    add: Added ProductNotFoundException

---
+++ catalog-ms/src/main/java/br/com/onlineStore/catalogms/core/exception/ProductNotFoundException.java
@@ -0,0 +1,6 @@
+package br.com.onlineStore.catalogms.core.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+
+public class ProductNotFoundException extends EntityNotFoundException {
+}
---
+++ shoppingCart-ms/src/main/java/br/com/onlineStore/shoppingCartms/core/exception/ProductNotFoundException.java
@@ -0,0 +1,6 @@
+package br.com.onlineStore.shoppingCartms.core.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+
+public class ProductNotFoundException extends EntityNotFoundException {
+}
