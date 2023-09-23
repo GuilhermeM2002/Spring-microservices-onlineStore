764a31be5bfcaeb830ce61712df6d7a532941f36
Guilherme Martins <guilhermemartinsribeiro2002@gmail.com>
2023-09-23 17:25:31 -0300

    hotfix: fixed bug when create shoppingCart table

--- shoppingCart-ms/src/main/resources/db/migration/V1__create_table_shopping_cart.sql
+++ shoppingCart-ms/src/main/resources/db/migration/V1__create_table_shopping_cart.sql
@@ -1,5 +1,6 @@
 create table shopping_cart(
     id bigint not null auto_increment,
+    product_fk bigint not null,
     name varchar(40) not null,
     price numeric(6, 2) not null,
     quantity int not null,
--- shoppingCart-ms/src/main/resources/db/migration/V4__alter_table_shopping_cart_add_foreign_key_product.sql
+++ shoppingCart-ms/src/main/resources/db/migration/V4__alter_table_shopping_cart_add_foreign_key_product.sql
@@ -1,2 +1,1 @@
-alter table shopping_cart add column product_fk bigint not null;
 alter table shopping_cart add constraint shopping_cart_product_fk foreign key (product_fk) references product (id);
